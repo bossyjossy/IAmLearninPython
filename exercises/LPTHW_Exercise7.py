3874714df2a651680f423223443ff30bc8fa35c8
Joss <climbnbike@yahoo.com>
2016-06-19 21:41:27 -0700

    Exercise 7 added
    Exercise 7 - more on printing

---
+++ exercises/LPTHW_Exercise7.py
@@ -0,0 +1,29 @@
+# This is my completed exercise for Learn Python The Hard Way Exercise 7
+#
+# This is slightly modified from the lesson so that I can test things out
+# and really solidify what I learned.
+#
+# EXERCISE 7: More Printing
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+# This next line will print 10 periods.
+print "." * 10 
+
+end1 = "C"
+end2 = 'h'
+end3 = "E"
+end4 = 'e'
+end5 = "s"
+end6 = 'E'
+end7 = "b"
+end8 = 'u'
+end9 = "r"
+end10 = 'G'
+end11 = "e"
+end12 = 'r'
+
+# The comma at the end of the first line ensures that the second line prints on the same line.
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
